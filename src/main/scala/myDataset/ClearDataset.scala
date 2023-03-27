f1c98d3576d6792166f92c154c67ac175bc0ddf8
MarPerez <marceloperez9210@gmail.com>
2023-03-27 14:45:15 -0300

    Fix in dataset.

---
+++ src/main/scala/myDataset/ClearDataset.scala
@@ -0,0 +1,21 @@
+package myDataset
+
+import org.apache.spark.sql.SparkSession
+
+object ClearDataset extends App{
+  val spark = SparkSession.builder()
+    .appName("Remove double quotes")
+    .master("local[*]")
+    .getOrCreate()
+
+  val fileName = "src/main/resources/data/used_cars_data.csv"
+  val linesRDD = spark.sparkContext.textFile(fileName)
+
+  val newLinesRDD = linesRDD.map(line => line.replaceAll("\"\"", ""))
+
+  val outputPath = "src/main/resources/data/newCleanDataset.csv"
+  newLinesRDD.saveAsTextFile(outputPath)
+
+  // Cierra la sesión de Spark
+  spark.stop()
+}
